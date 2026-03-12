218148ec93ab9d5d207b3ca24d1186415ac7a6c6
YINNSTORE <146443900+YINNSTORE@users.noreply.github.com>
2026-03-12 10:45:56 +0700

    Create app/src/main/java/com/gptnmanager/ui/theme/Color.kt

---
+++ app/src/main/java/com/gptnmanager/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.gptnmanager.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Navy = Color(0xFF0A1931)
+val NavySurface = Color(0xFF102544)
+val Blue = Color(0xFF2F6BFF)
+val LightBg = Color(0xFFF5F7FC)
