e76d73f9c8668b883d2b13307d6ed8ca6b7f1f85
YINNSTORE <146443900+YINNSTORE@users.noreply.github.com>
2026-03-12 13:02:58 +0700

    Update Color.kt

--- app/src/main/java/com/gptnmanager/ui/theme/Color.kt
+++ app/src/main/java/com/gptnmanager/ui/theme/Color.kt
@@ -2,7 +2,17 @@ package com.gptnmanager.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Navy = Color(0xFF0A1931)
-val NavySurface = Color(0xFF102544)
-val Blue = Color(0xFF2F6BFF)
-val LightBg = Color(0xFFF5F7FC)
+val Navy = Color(0xFF0B1730)
+val NavySoft = Color(0xFF132347)
+val NavyCard = Color(0xFF182A52)
+val NavyCardSoft = Color(0xFF213764)
+val BlueAccent = Color(0xFF4C7DFF)
+
+val LightBg = Color(0xFFF4F7FC)
+val LightCard = Color(0xFFEAF0FF)
+val LightCardSoft = Color(0xFFF1ECFA)
+
+val SuccessColor = Color(0xFF12B76A)
+val ErrorColor = Color(0xFFF04438)
+val InfoColor = Color(0xFF1570EF)
+val WarningColor = Color(0xFFF79009)
